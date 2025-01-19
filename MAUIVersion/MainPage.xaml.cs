265c34233d2c6b3aad4572aeffd7e8d9e4ba396b
iris <scout20010@gmail.com>
2025-01-19 20:01:36 +0300

    Добавьте файлы проекта.

---
+++ MAUIVersion/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MAUIVersion
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void DisplayAlertClick(object sender, EventArgs e)
+        {
+            DisplayAlert("Нажата кнопка", "Вы нажали кнопку", "ОК");
+        }
+    }
+
+}
